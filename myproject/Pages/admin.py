c887f0357bff6453d15e640886515814d7ea82a5
omi Patel <omipatel213@gmail.com>
2022-05-26 13:24:06 +0530

    changes in add form

--- myproject/Pages/admin.py
+++ myproject/Pages/admin.py
@@ -1,4 +1,6 @@
+import re
 from django.contrib import admin
+from django.shortcuts import redirect, render
 from Pages.models import *
 from Language.models import *
 import json
@@ -25,15 +27,37 @@ class PagesAdmin(admin.ModelAdmin):
             )
         page_data = (
             Pages.objects.all().values()
-        )
+            )
         if obj == None:
             Pages_data = (
                 PageLanguage.objects.raw("select * from pages_pagelanguage")
-            )
+                )
+                
+            if request.method == 'POST':
+                slug = request.POST['slug']
+                sortorder = request.POST['sortorder']
+                status = request.POST['status'] 
+
+                page = Pages(slug=slug,sortorder=sortorder,status=status)
+                page.save()
+
+                pagesdata = Pages.objects.get(slug = slug)
+
+
+                for i in lang_data: 
+                    title = request.POST[i.locale+'title'] 
+                    content = request.POST[i.locale+'content']
+                    lang = request.POST[i.locale+'language']
+                    langdata = Language.objects.get(locale = lang)
+
+                    PageLanguagedata = PageLanguage(title = title, content = content, pages = pagesdata, language = langdata)
+                    PageLanguagedata.save()
+
         else:
             Pages_data = (
                     PageLanguage.objects.raw("select * from pages_pagelanguage where pages_id = '"+obj+"'")
                 )
+
         extra_context = extra_context or {"lang_data": lang_data,"Pages_data":Pages_data,"page_data":page_data,"obj":obj}
         return super().changeform_view(request, obj,form_url,extra_context=extra_context)
 
